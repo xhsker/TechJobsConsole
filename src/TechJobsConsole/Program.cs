d5791cac654fb7f15b2df196ab4a32f8ae386417
Tom Lake <xhsker@sbcglobal.net>
2018-11-14 19:43:11 -0600

    Fixed Code

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -117,8 +117,30 @@ namespace TechJobsConsole
         }
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
-        {
-            Console.WriteLine("printJobs is not implemented yet");
+        {            
+            
+            //Console.WriteLine("printJobs is not implemented yet");
+
+            int count = someJobs.Count;
+            if (count == 0)
+            {
+                Console.WriteLine("No information found with the search");
+            }
+            else
+            {
+                foreach (Dictionary<string, string> item in someJobs)
+                {
+                    Console.WriteLine("\n****");
+                    foreach (KeyValuePair<string, string> itemline in item)
+                    {
+                        if (itemline.Key != "S.no")
+                            Console.WriteLine(itemline.Key + " : " + itemline.Value);
+                    }
+                    Console.WriteLine("****\n\n");
+                }
+            }
+
+
         }
     }
 }
